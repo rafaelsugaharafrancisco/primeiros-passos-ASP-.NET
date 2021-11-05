97f6470b0224b1973633667a0d73a833eec80df5
Rafael <rafa.s.francisco@gmail.com>
2021-11-05 19:54:04 -0300

    primeiro commit

---
+++ Models/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCurso.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        [
+            Display(Name = "Descrição"), 
+            Required(ErrorMessage = "Campo obrigatório"),
+            StringLength(15, MinimumLength = 5, ErrorMessage = "O campo deve ter entre 5 e 15")
+        ]
+        public string Descricao { get; set; }
+
+        public List<Produto> Produtos { get; set; }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCurso.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [
+            Display(Name = "Descrição"),
+            Required(ErrorMessage = "Campo obrigatório"),
+            StringLength(15, MinimumLength = 1, ErrorMessage = "O campo deve ter entre 1 e 15")
+        ]
+        public string Descricao { get; set; }
+
+        [
+            Required(ErrorMessage = "Campo obrigatório"),
+            Range(1, 50, ErrorMessage = "Deve ter a quantidade entre 1 e 50")
+        ]
+        public int Quantidade { get; set; }
+
+        public int CategoriaId { get; set; }
+
+        public Categoria Categoria { get; set; }
+    }
+}
